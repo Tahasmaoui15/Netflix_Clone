3e0573980b151db82a97b0e32cbb822f051758ce
Taha Smaoui <tahasmaoui1993@gmail.com>
2024-01-26 18:29:22 +0100

    Fetched all movies categories in App.js & Added style to the movies posters in Row.css.

--- src/Row.js
+++ src/Row.js
@@ -26,6 +26,7 @@ const Row = ({ title, fetchUrl }) => {
       <div className="row__posters">
         {movies.map((movie) => (
           <img
+            key={movie.id}
             className="row__poster"
             src={`${base_url}${movie.poster_path}`}
             alt={movie.name}
--- src/requests.js
+++ src/requests.js
@@ -8,7 +8,7 @@ const requests = {
     fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
     fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
     fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
-    fetcDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`
+    fetcDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
 }
 
 export default requests;
