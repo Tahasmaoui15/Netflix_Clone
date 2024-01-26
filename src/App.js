86f7caee216b07865e399d0eaa4da3155f4b2868
Taha Smaoui <tahasmaoui1993@gmail.com>
2024-01-26 19:22:14 +0100

    Removed <h3> from app.js & Added a margin to the rows.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import requests from "./requests";
 function App() {
   return (
     <div className="App">
-      <h3>Hi</h3>
+      
       <Row
         title="NETFLIX ORIGINALS"
         fetchUrl={requests.fetchNetflixOriginals}
