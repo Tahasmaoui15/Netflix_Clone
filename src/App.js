000856d4465a691900b193ce23cd2012e040b161
Taha Smaoui <tahasmaoui1993@gmail.com>
2024-01-30 18:07:26 +0100

    Added Nav.js and Nav.css to src & Imported Nav.js to App.js after adding the Netflix logo and avatar to the navbar.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 import Row from "./Row";
 import { requests } from "./Requests";
 import Banner from "./Banner";
+import Nav from "./Nav";
 
 function App() {
   return (
     <div className="app">
+      <Nav />
       <Banner />
       <Row
         title="NETFLIX ORIGINALS"
---
+++ src/Nav.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <div className="nav">
+      <img
+        className="nav__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/1/15/Logonfx.png"
+        alt="Netflix Logo"
+      />
+
+      <img
+        className="nav__avatar"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+        alt="Avatar"
+      />
+    </div>
+  );
+}
+
+export default Nav;
